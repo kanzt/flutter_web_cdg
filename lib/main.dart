3bd2ee3f6cd2a3ed7e765842fd072f811d1bdfcc
kanzt <kanzttt@gmail.com>
2024-05-16 09:45:22 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_101/src/main/presentation/widgets/basic/playground.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      // home: const MyHomePage(),
+      home: const Playground(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    /// UI Template ที่จะมีการจัด Layout เตรียมไว้ล่วงหน้า
+    return const SafeArea(
+      child: Scaffold(),
+    );
+  }
+}
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class LargeChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SmallChild extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/main/presentation/widgets/basic/playground.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_web_101/src/res/drawable/drawable.dart';
+
+class Playground extends StatelessWidget {
+  const Playground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildRichText(),
+    );
+  }
+
+  _buildRow() {
+    return SizedBox(
+      // color: Colors.red,
+      width: double.infinity,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildColumn() {
+    return SizedBox(
+      // color: Colors.red,
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildStack() {
+    return Container(
+      // color: Colors.red,
+      width: double.infinity,
+      child: Stack(
+        alignment: AlignmentDirectional.topStart,
+        children: [
+          Container(
+            color: Colors.orange,
+            child: const FlutterLogo(
+              size: 80.0,
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: const FlutterLogo(
+              size: 70.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: const FlutterLogo(
+              size: 60.0,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildContainer() {
+    return Container(
+      width: 48,
+      height: 48,
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.amber[600],
+      ),
+    );
+  }
+
+  _buildPadding() {
+    return Container(
+      color: Colors.red,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: _buildContainer(),
+      ),
+    );
+  }
+
+  _buildText() {
+    return const Text(
+      "Hello World",
+      style: TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+        fontSize: 40,
+      ),
+    );
+  }
+
+  _buildRichText() {
+    return RichText(
+      text: const TextSpan(
+          text: "Hello ",
+          style: TextStyle(fontSize: 60, color: Color(0xFF8591B0)),
+          children: [
+            TextSpan(
+                text: "World",
+                style: TextStyle(
+                    fontSize: 60,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red))
+          ]),
+    );
+  }
+
+  _buildImage() {
+    return Image.asset(
+      Drawable.logo,
+      width: 200,
+      height: 200,
+    );
+  }
+}
